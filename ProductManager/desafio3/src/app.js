8ac30a40c63ccb410b37695bb06c62bd1a3e0372
criistiian10 <chriistianariel@outlook.com>
2024-02-21 22:33:12 -0300

    termine el 2do proyecto

--- ProductManager/desafio3/src/app.js
+++ ProductManager/desafio3/src/app.js
@@ -1,64 +1,44 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import { engine } from "express-handlebars";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import mongoose from "mongoose";
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
+import __dirname from "./utils.js";
 
-import viewsRouter from "./routes/views.routes.js";
-import registroRouter from "./routes/registro.routes.js";
-import loginRouter from "./routes/login.routes.js";
-import productsRoutes from "./routes/product.routes.js";
-import cartRoutes from "./routes/cart.routes.js";
+import usersRouter from "./routes/users.router.js";
+import coursesRouter from "./routes/courses.router.js";
+import sessionsRouter from "./routes/sessions.router.js";
+import viewsRouter from "./routes/views.router.js";
+
+import handlebars from "express-handlebars";
+import mongoose from "mongoose";
+import passport from "passport";
+import initializePassport from "./config/passport.config.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = 8080;
-const USER_MONGO = process.env.USER_MONGO;
-const PASSWORD_MONGO = process.env.PASSWORD_MONGO;
-const DB_MONGO = process.env.DB_MONGO;
-const DB_SERVER = process.env.DB_SERVER;
-
-const DB_URL = `mongodb+srv://${USER_MONGO}:${PASSWORD_MONGO}@${DB_SERVER}/${DB_MONGO}?retryWrites=true&w=majority`;
-app.use(express.json());
-
-app.engine("handlebars", engine());
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/test";
+const connection = mongoose.connect(DB_URL);
+/**
+ * Template engine
+ */
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
-app.set("views", "./src/views");
-app.use(express.static("public"));
 
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+/**
+ * Middlewares
+ */
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+initializePassport();
+app.use(passport.initialize());
 app.use(cookieParser());
 
-app.use(
-  session({
-    store: MongoStore.create({
-      mongoUrl: DB_URL,
-      mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-      ttl: 10000,
-    }),
-    secret: "codigo-s3cr3t0",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
-//coneccion a mongoose
-mongoose.set("strictQuery", false);
-mongoose.connect(DB_URL, (err) => {
-  if (err) {
-    console.log("No se puede conectar a la base de datos");
-  } else {
-    console.log("Mongoose conectado con Exito");
-  }
-});
-
 app.use("/", viewsRouter);
-app.use("/api/registro", registroRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/products", productsRoutes);
-app.use("/api/carts", cartRoutes);
+app.use("/api/users", usersRouter);
+app.use("/api/courses", coursesRouter);
+app.use("/api/sessions", sessionsRouter);
+
+const server = app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
